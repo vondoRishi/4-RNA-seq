e70c66a533f1b8098e75fd2d33d37eb6460b86f7
Rishi Das Roy <rishi.dasroy@gmail.com>
2019-03-15 16:21:41 +0200

    Update trimmo.sh

--- scripts/trimmo.sh
+++ scripts/trimmo.sh
@@ -26,11 +26,10 @@ do
 	filename="${my_file##*/}"
 	extension="${filename##*.}"
 	filename="${filename%.*}"
-	echo $filename ;
-	echo $extension ;
   echo "trimmomatic SE -phred33 -threads 8  $my_file $2/trimmed_$filename.$extension \
 ILLUMINACLIP:/appl/bio/trimmomatic/adapters/TruSeq3-SE.fa:2:30:10   " >> commands/$num_cmnds"_Trimmomatic_"$1_commands.txt
 done
+ 
  sbatch_commandlist -t 1:00:00 -mem 4000 -jobname trimo_array -threads 8 -commands commands/$num_cmnds"_Trimmomatic_"$1_commands.txt
 
 mv *_out_*txt OUT
